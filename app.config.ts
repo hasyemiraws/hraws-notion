60e2c9bd7707963ed6b640037efdef421a67081e
Hasyemi Rafsanjani Asyari <hasyemirafsanjaniasyari@gmail.com>
2022-12-16 21:20:45 +0700

    chore: initial commit

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+    title: 'Journal of High Functional Introvert',
+    theme: {
+        dark: true,
+        colors: {
+            primary: '#ff0000'
+        }
+    }
+})
